b969e4e3ca44ca66b5f6aa106348b7770f6b84a1
donghyuc-kim <msoopul@gimdonghyeog-ui-MacBook-Pro.local>
2016-02-28 17:19:39 +0900

    백업

---
+++ scripts/referrer.js
@@ -0,0 +1,6 @@
+
+var widgetNavigatorIndexElem;
+if(document.referrer.indexOf('donghyuc.com') < 0) {
+	widgetNavigatorIndexElem = document.getElementById('widget-navigator--index'); 
+	widgetNavigatorIndexElem.className = '_includes';	
+}
